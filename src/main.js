7e1b0e69b2fe777e74a92255c5e8c13127aed50c
ketsarin <ketsarin.sontisuk@gmail.com>
2020-02-18 17:52:24 +0100

    Add external lib BootstrapVue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
 // Set up API base URL
 axios.defaults.baseURL = 'https://rickandmortyapi.com/api';
 
 Vue.config.productionTip = false
+
+// Register BootstrapVue
+import { BootstrapVue } from 'bootstrap-vue'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
 
 // Import components.
 require('./components/characters');
