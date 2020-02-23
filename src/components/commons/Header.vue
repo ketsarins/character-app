9fe13d61f7549b0318ace784183a394546ed8b78
ketsarin <ketsarin.sontisuk@gmail.com>
2020-02-23 20:05:59 +0100

    Add home button to go back to character list page

--- src/components/commons/Header.vue
+++ src/components/commons/Header.vue
@@ -1,13 +1,17 @@
 <template>
   <header class="header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
-      <div class="col-4 pt-1"></div>
+      <div class="col-4 pt-1">
+        <b-button variant="success" v-if="!characterPage()" v-on:click="goToHomePage()">
+          <b-icon-house></b-icon-house>&nbsp;Home
+        </b-button>
+      </div>
       <div class="col-4 text-center">
         <h2 class="header-logo text-dark">Rick and Morty</h2>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
         <b-button v-b-toggle.fillter-collapse variant="outline-success" v-if="characterPage()">
-          <b-icon-filter></b-icon-filter>Fillter
+          <b-icon-filter></b-icon-filter>&nbsp;Fillter
         </b-button>
       </div>
     </div>
@@ -18,8 +22,10 @@ export default {
   name: "app-header",
   methods: {
     characterPage() {
-      //
       return this.$route.path === "/";
+    },
+    goToHomePage() {
+      this.$router.push({name: "CharaterPage"});
     }
   }
 };
